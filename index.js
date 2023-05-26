5c899f9785d52a79db6aa57c8fa1643f3d791c68
Rahul Kumar Meenaa <99737908+royrahulrk@users.noreply.github.com>
2023-05-26 11:40:24 +0530

    update

--- index.js
+++ index.js
@@ -1,38 +1,25 @@
-const express=require('express');
-const socket=require('socket.io');
-const PORT= process.env.PORT ||8080;
-const path= require('path');
-
-const app=express();
-app.use(express.static(path.join(__dirname,'./public')));
-
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'./public/index.html');
-})
-
-const server = app.listen(PORT,function(){
-    console.log(`I am listening to the port ${PORT}`);
-})
-
-const io = socket(server,{cors: {origin:"*"}});
-
-const users = {};
-
-io.on('connection',socket=>{
-    //user joins
-    socket.on('new-user-joined', name=>{
-        console.log(name,'joined the chat.');
-        users[socket.id]=name;
-        socket.broadcast.emit('user-joined',name);
+const io = require('socket.io')({
+    cors: {
+      origin: '*',
+    },
+  });
+  
+  io.on('connection', (socket) => {
+    console.log('user connected');
+  
+    socket.on('chat', (data) => {
+      console.log(`message received: ${data.message}`);
+      io.emit('message', {
+        username: data.username,
+        message: data.message,
+      });
     });
-    //message is sent
-    socket.on('send',message=>{
-        socket.broadcast.emit('receive',{message:message,name:users[socket.id]});
-    })
-    //when we want to disconnect
-    socket.on('disconnect',message=>{
-        socket.broadcast.emit('left',users[socket.id]);
-        delete users[socket.id];
-    })
-    
-})
+  
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+  
+  const port = 5000;
+  io.listen(port);
+  console.log(`Server listening on port ${port}`);
